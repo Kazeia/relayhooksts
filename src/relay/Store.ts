54fa5b709b9061714f32c119af71539063ca2dd1
Kazeia <davidsilvasolano@gmail.com>
2019-12-21 01:47:57 -0500

    ADD TODO

--- src/relay/Store.ts
+++ src/relay/Store.ts
@@ -9,6 +9,7 @@ export default function (modernEnvironment: Environment): Environment {
       dataID = `client:${__typename}`,
       record = store.create(dataID, __typename);
 
+    // TODO: use localstorage as demand: https://babangsund.com/relay_local_state_management_2/
     record.setValue('123233', 'id')
 
     store.getRoot().setLinkedRecord(record, fieldKey)
